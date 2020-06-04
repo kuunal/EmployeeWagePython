1dea87f3be84884e0427626d07acd5cf7f2c3f9d
250399 <kunaldeshmukh2503@gmail.com>
2020-06-03 18:58:58 -0700

    ADDED: code for calculating daily full day wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,7 +1,29 @@
 import random
 
-random_attendance = round(random.random())
-if random_attendance == 1:
-    print("Employee is present")
-else:
-    print("Employee is absent") 
+class Constant:
+    _WAGE_PER_HOUR = 20  
+    _FULL_DAY_HOUR = 8
+
+    @property
+    def WAGE_PER_HOUR(self):
+        return self._WAGE_PER_HOUR
+
+    @property
+    def FULL_DAY_HOUR(self):
+        return self._FULL_DAY_HOUR
+
+class Employee:
+    def check_employee(self):
+        random_attendance = round(random.random())
+        if random_attendance == 1:
+            print("Employee is present")
+            print("Full day wage is :",self.calculate_fullday_wage())
+        else:
+            print("Employee is absent") 
+
+    def calculate_fullday_wage(self):
+        constant = Constant()
+        return constant.WAGE_PER_HOUR * constant.FULL_DAY_HOUR
+
+employee = Employee()
+employee.check_employee()
